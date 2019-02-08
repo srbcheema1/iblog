30000225c74e9c30952cb8951abf4b17bc049129
Sarbjit Singh <srbcheema1@gmail.com>
2019-02-08 05:41:30 +0530

    login and register working, able to login and logout
    connected to db
    working fine

---
+++ iblog.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# PYTHON_ARGCOMPLETE_OK
+
+from iblog import app
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ iblog/routes.py
@@ -0,0 +1,84 @@
+from srblib import str_hash
+from srblib import debug
+
+from flask import render_template, url_for, flash, redirect, request
+from flask_login import login_user, current_user, logout_user, login_required
+
+from .forms import RegistrationForm, LoginForm
+from .models import User, Post
+from .config import app, db
+# from . import app, db
+
+
+posts = [
+    {
+        'author': 'Corey Schafer',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2018'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 21, 2018'
+    }
+]
+
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html', posts=posts)
+
+
+@app.route("/about")
+def about():
+    return render_template('about.html', title='About')
+
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        hashed_pswd = str_hash(form.password.data)
+        user = User(username=form.username.data, email=form.email.data, password=hashed_pswd)
+        db.session.add(user)
+        db.session.commit()
+        flash('Account created for '+form.username.data+'!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        hashed_pswd = str_hash(form.password.data)
+        if user and hashed_pswd == user.password:
+            login_user(user, remember=form.remember.data)
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('home'))
+        if(debug):
+            if not user:
+                flash('Email '+form.email.data+' not registered', 'danger')
+            else:
+                    flash('Incorrect password ', 'danger')
+        else:
+            flash('Login Unsuccessful. Please check email and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
+
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+
+@app.route("/account")
+@login_required
+def account():
+    return render_template('account.html', title='Account')
