9e581d9733cab19651635599d42c8c9b72cf0e4c
Sarbjit Singh <srbcheema1@gmail.com>
2019-02-13 06:27:11 +0530

    work on editors

--- iblog/utils.py
+++ iblog/utils.py
@@ -1,5 +1,7 @@
 from iblog.const import home_post_len
+from bs4 import BeautifulSoup
 def prefix(string,l=home_post_len):
+    string = BeautifulSoup(string,'html.parser').text
     if len(string) < l:
         return string
     else:
